453185451e3da0cacd05b4bda94559f320a09226
Lincxx <bossco28@gmail.com>
2021-10-18 06:43:15 -0400

    first commit

---
+++ src/views/AnotherPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello from another page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+       name:'AnotherPage' 
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

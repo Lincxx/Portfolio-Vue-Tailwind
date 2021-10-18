f6fc9ce1527c3114976de35cce504993a7093931
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-10-18 16:58:56 -0400

    Added in a card element

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,8 +1,17 @@
 <template>
 <div>
     <NavBar :ishome="false"/>
-    <div class="pt-24">
-        hello
+    <div class="pt-24 min-h-screen w-screen top flex flex-col flex-wrap justify-center items-center px-8 bg-gray-200">
+        <div class="w-full flex flex-wrap">
+            <div class="w-1/4 p-2">
+                <div class="bg-white rounded shadow-xl">
+                    <img src="" alt="">
+                    <div class="px-6 py-4">
+                        <p class="font-bold text-gray-900 text-lg mb-2">Project Name</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
     

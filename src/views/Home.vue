1cbf99c9ac4ea96e00f769915152ac8da0423a26
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-10-18 13:18:31 -0400

    Added Styling to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="home">
-    <h1>Hello</h1>
+  <div>
+      <div class="relative h-screen w-screen bg-cover bg-center shadow-2xl" :style="`background-image:url(${bgImage})`">
+      <div class="h-screen w-screen bg-red-800 opacity-75"></div> 
+      <div class="absolute h-screen w-screen top-0 flex flex-col items-center justify-center">
+        <h1 class="text-6xl font-bold text-white p-4 hover:bg-white hover:text-red-800">Jeremy Lincoln</h1>
+        <h3 class="text-2xl font-bold text-white px-4 hover:bg-white hover:text-red-800">Development Team Lead / Front End Developer</h3>
+      </div>
+      
+      </div>    
   </div>
 </template>
 
 <script>
+import bgImage from '../assets/bg.jpg'
 export default {
   name: "Home",
+  data() {
+    return {
+      bgImage
+    }
+  }
 };
 </script>

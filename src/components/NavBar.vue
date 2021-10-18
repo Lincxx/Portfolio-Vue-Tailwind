2190d9608e552d92f4dfd625fe1595d3125c583e
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-10-18 12:40:49 -0400

    Added in Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="nav w-screen z-20 shadow-2xl bg-red-800 text-white" >
+       <div class="left">
+           <span class="font-light text-3xl">
+               My
+               <span class="font-semibold">Portfolio</span>
+           </span>
+       </div>
+       <div class="right">
+           <router-link class="link" to="/">Home</router-link>
+           <router-link class="link" to="/">Portoflio</router-link>
+       </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<style lang="scss" scoped>
+/**apply is only for tailwind*/
+    .nav {
+        @apply h-24 fixed px-12 flex justify-between items-center ;
+    }
+    .link {
+        @apply px-2 font-semibold;
+        &:hover {
+            @apply text-yellow-500
+        }
+    }
+</style>

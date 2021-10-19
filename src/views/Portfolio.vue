a15240c6325ef7c1b402d5663eb35077f8278f80
Lincxx <bossco28@gmail.com>
2021-10-19 06:45:06 -0400

    Added a card to portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -8,6 +8,18 @@
                     <img src="" alt="">
                     <div class="px-6 py-4">
                         <p class="font-bold text-gray-900 text-lg mb-2">Project Name</p>
+                        <p class="text-gray-700 text-base mb-4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi unde consequatur, ab eum exercitationem laborum, explicabo optio quaerat molestiae eos dolor, cupiditate placeat delectus est.
+                        </p>
+                        <a href="" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Open</a>
+                    </div>
+                    <div class="px-6 py-4">
+                        <span 
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-2"
+                            >
+                            #somehash
+                            https://youtu.be/aaaFahk2dOw?t=1218
+                        </span>
                     </div>
                 </div>
             </div>

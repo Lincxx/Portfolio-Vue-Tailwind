008c6ec858bd51b306cf5319705db1183e22147a
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-10-19 11:12:16 -0400

    Added in contect to the card

---
+++ src/data/project.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        name: 'Basic Todo App',
+        description: 'A Vanilla HTML, JS and CSS Todo Application',
+        image: 'https://via.placeholder.com/250',
+        hashtags: ['todos', 'html5', 'css', 'javascipt'],
+        url: ''
+    },
+    {
+        name: 'Todo App with Tailwind',
+        description: 'A Todo Application in Vanilla Javascript and Tailwind',
+        image: 'https://via.placeholder.com/250',
+        hashtags: ['todos', 'html5', 'tailwind', 'javascipt'],
+        url:''
+    },
+    {
+        name: 'Basic Inventory App in Vue.js',
+        description: 'A Basic Inventory App in Vue.js',
+        image: 'https://via.placeholder.com/250',
+        hashtags: ['inventory', 'html5', 'css', 'vue', 'tailwind'],
+        url:''
+    },
+    {
+        name: 'Todo App with Tailwind and Vue.js',
+        description: 'A Todo App with Tailwind and Vue.js',
+        image: 'https://via.placeholder.com/250',
+        hashtags: ['todos', 'html5', 'css', 'javascipt'],
+        url:''
+    }
+]
+
+export default projects
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,28 +1,12 @@
 <template>
 <div>
     <NavBar :ishome="false"/>
-    <div class="pt-24 min-h-screen w-screen top flex flex-col flex-wrap justify-center items-center px-8 bg-gray-200">
+    <div class="pt-24 min-h-screen w-screen top flex flex-col flex-wrap justify-center items-center px-8 bg-gray-200 shadow-lg">
         <div class="w-full flex flex-wrap">
-            <div class="w-1/4 p-2">
-                <div class="bg-white rounded shadow-xl">
-                    <img src="" alt="">
-                    <div class="px-6 py-4">
-                        <p class="font-bold text-gray-900 text-lg mb-2">Project Name</p>
-                        <p class="text-gray-700 text-base mb-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi unde consequatur, ab eum exercitationem laborum, explicabo optio quaerat molestiae eos dolor, cupiditate placeat delectus est.
-                        </p>
-                        <a href="" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Open</a>
-                    </div>
-                    <div class="px-6 py-4">
-                        <span 
-                            class="inline-block bg-gray-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-2"
-                            >
-                            #somehash
-                            https://youtu.be/aaaFahk2dOw?t=1218
-                        </span>
-                    </div>
-                </div>
+            <div class="w-1/4 p-2 mb-8" v-for="(project, index) in projects" :key="index">
+               <Card :project="project"/> 
             </div>
+            
         </div>
     </div>
 </div>
@@ -31,11 +15,19 @@
 
 <script>
     import NavBar from '../components/NavBar'
+    import Card from '../components/Card'
+    import projects from '../data/project'
     export default {
+        name: 'Portfolio',
         components: {
             NavBar,
+            Card
         },
-       name: 'Portfolio' 
+        data() {
+            return {
+                projects: projects
+            }
+        },
     }
 </script>
 
